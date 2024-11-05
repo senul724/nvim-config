3d629af801f64f3da6c7fa43ced68c800a1bf0a3
senul ranasinghe <hardlove.mrplus@gmail.com>
2024-11-05 17:14:09 +0530

    fix(options): enabled arrow keys

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -53,10 +53,10 @@ vim.keymap.set("n", "<leader>`", "<cmd>b#<CR>", {
 })
 
 -- disable arrow keys in the normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+-- vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
